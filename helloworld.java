53a86147bfa85f29306d81465d06b900e30cbb29
raphaellebas <raphael.lebas@outlook.fr>
2021-12-01 10:58:10 +0100

    Premier essaie d'un commit

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld {
+
+	public static void main(String[] args) {
+		// Va imprimé "Hello World" dans le Terminal -- Print "Hello World" to the Terminal
+		System.out.println("Hello World");
+	}
+}
