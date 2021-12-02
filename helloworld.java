c420194754e1fb2a5bb3e88e96eea06558cf0105
raphaellebas <raphael.lebas@outlook.fr>
2021-12-02 13:58:10 +0100

    test commit3

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// Va imprimé "Bonjour tout le monde dans le terminal" -- Print "Bonjour tout le monde to the terminal"
-		System.out.println("Bonjour tout le monde ;)")
+		System.out.println("Bonjour tout le monde :3")
 	}
 }
